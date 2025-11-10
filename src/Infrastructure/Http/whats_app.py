d3abffca631b7822065bdf025e4581fb05164978
Guibellemo <gbellemo@gmail.com>
2025-11-10 12:23:21 -0300

    token twilio

--- src/Infrastructure/Http/whats_app.py
+++ src/Infrastructure/Http/whats_app.py
@@ -22,10 +22,11 @@ def send_whatsapp_code(code:str,phone:str):
     print(message.sid)
 '''
 
-TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+
 
 def send_sms_code(code: str, phone: str):
+    TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+    TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
     account_sid = TWILIO_ACCOUNT_SID
     auth_token = TWILIO_AUTH_TOKEN
     client = Client(account_sid, auth_token)
